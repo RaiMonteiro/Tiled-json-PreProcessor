3892d151eba355c96a589395a5d649ed9bce81b4
Raí Monteiro <rairibaesmonteiro@gmail.com>
2025-09-27 20:02:05 +0100

    Recommendation added

--- usage.py
+++ usage.py
@@ -1,5 +1,6 @@
 import TiledPreProcessor
 
 processor = TiledPreProcessor('assets/data/maps/tiled/collisions.json') # enter the relative path here
+# processor = TiledPreProcessor('assets/data/maps/tiled/collisions.json', True, Ture) # recommendation for a better approach
 processor.read_data_file()
 processor.store_data('test') # you can change the file name here
